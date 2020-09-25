c3de52c93de8aa7bad57047a4b591f3cfbd3e262
piya03 <piyachaudhary50@gmail.com>
2020-09-25 22:09:34 +0530

    quiz in progress

--- src/Component/Quiz.js
+++ src/Component/Quiz.js
@@ -5,40 +5,52 @@ import data from "./data";
 console.log("data", data.length);
 function Quiz() {
   const [activeIndex, setActiveIndex] = useState(0);
+  console.log("Quiz -> activeIndex", activeIndex);
+
   const [one, setOne] = useState("");
-  console.log("Quiz -> ans", one);
+  const [ans, setAns] = useState([]);
+  const [result, setResult] = useState(false);
+  console.log("Quiz -> ans", ans);
+  console.log("Quiz -> one", one);
 
   function next() {
     if (activeIndex < data.length - 1) {
       setActiveIndex((prev) => prev + 1);
     } else {
+      setResult(true);
       setActiveIndex(0);
     }
   }
   return (
     <div className="quiz_container">
       <h1>hello quiz</h1>
-      <div>
-        <p>{data[activeIndex]?.question}</p>
-        <div className="option_container">
-          {data[activeIndex].options.map((item, i) => {
-            return (
-              <CommonRadioBtn
-                value={item.id}
-                id={item.id}
-                htmlfor={item.id}
-                labelVal={item.label}
-                checked={one === item.id ? true : false}
-                onChange={(e) => {
-                  console.log("Quiz -> e", e.target.value);
-                  setOne(e.target.value);
-                }}
-                name="samename"
-              />
-            );
-          })}
+      {!result ? (
+        <div>
+          <p>{data[activeIndex]?.question}</p>
+          <div className="option_container">
+            {data[activeIndex].options.map((item, i) => {
+              return (
+                <CommonRadioBtn
+                  key={i}
+                  value={item.id}
+                  id={item.id}
+                  htmlfor={item.id}
+                  labelVal={item.label}
+                  checked={one === item.id ? true : false}
+                  onChange={(e) => {
+                    console.log("Quiz -> e", e.target.value);
+                    setOne(e.target.value);
+                    setAns([...ans, e.target.value]);
+                  }}
+                  name="samename"
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : (
+        <p>Your result</p>
+      )}
       <button onClick={next} className="btn">
         Next
       </button>
